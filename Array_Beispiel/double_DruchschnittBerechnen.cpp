2c30e77689c030bebfb7bd5a97bbb654466a889c
Mehmet Deha Balkis <49536918+dehabalkis@users.noreply.github.com>
2020-11-24 19:57:10 +0100

    Cpp_Ver_1.21_24_11_2020

---
+++ Array_Beispiel/double_DruchschnittBerechnen.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream> // Read funktion
+
+#include <string>// String Funktion
+
+#include<fstream> // Read Funktion
+
+#include <algorithm> //Matematische Funktion
+
+#include <vector> // Vector und Array ähnlich Funktion
+
+#include <array> // Array low lavel Software lösungen Funktion
+
+using namespace std; // std::cout << i << endl; bunu yazmamak icin bu formulu kullandik!
+
+
+int main()
+{
+    int n = 0;
+
+    array<double, 4> a = { 2.0, 3.0, 4.0, 5.0 };
+
+    double sum = 0.0;
+
+    for (const auto &element : a) {
+
+        sum = sum + element;
+
+    }
+
+    cout << sum / a.size() << endl;
+
+    cout << sum << endl;
+
+    return 0;
+}
